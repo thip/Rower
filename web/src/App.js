e88f48cf8fee5ff039d9246c9edc44d9574b72e4
David Capper <david@thecappers.co.uk>
2021-09-20 14:41:52 +0100

    Add react based UI

--- web/src/App.js
+++ web/src/App.js
@@ -7,8 +7,10 @@ function App() {
   const [setup, setSetup] = useState(true);
   const [targetTime, setTargetTime] = useState(0);
   const [targetDistance, setTargetDistance] = useState(0);
+  const [startTime, setStartTime] = useState();
 
   const onStart = ({distance, time}) => {
+    setStartTime(Date.now);
     setTargetTime(time);
     setTargetDistance(distance);
     setSetup(false);
@@ -16,7 +18,11 @@ function App() {
 
   return (
     <div style={{display: "flex", justifyContent: "center"}}>
-      { setup ? <Setup onStart={onStart}/> : <Action targetTime={targetTime} targetDistance={targetDistance}/> }
+      { 
+        setup 
+          ? <Setup onStart={onStart}/>
+          : <Action startTime={startTime} targetTime={targetTime} targetDistance={targetDistance}/>
+      }
     </div>
   );
 }
